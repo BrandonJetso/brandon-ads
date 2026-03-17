404be58d0134b19542ed68b3e6405164853da989
Brice Simoneau <thebrandonapp@gmail.com>
2026-03-17 10:05:16 +0800

    update site URL to workers.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     output: 'static',
-    site: 'https://brandon-ads.pages.dev',
+    site: 'https://brandon-ads.thebrandonapp.workers.dev',
 });
